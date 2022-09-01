38ee0db881279c79d3c0e1dd57282a821ffb2178
Nandu Dumare <dumarenandu70@gmail.com>
2022-09-01 21:24:53 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -1,34 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-const connection = require('./Database/db')
-const user = require('./Controller/user');
-const todo = require('./Controller/todo')
+const express = require("express");
+const cors = require("cors");
+const connection = require("./Database/db");
+const user = require("./Controller/user");
+const todo = require("./Controller/todo");
 
 const app = express();
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({
-    origin:"https://admirable-haupia-6a5610.netlify.app"
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://admirable-haupia-6a5610.netlify.app",
+    ],
+  })
+);
 
 app.get("/", (req, res) => {
-    res.send("hello")
-})
+  res.send("hello");
+});
 
+app.use("/user", user);
 
-app.use("/user", user)
-
-app.use("/todo",todo)
-
+app.use("/todo", todo);
 
 const PORT = process.env.PORT || 8080;
 
-
 app.listen(PORT, async () => {
-    try {
-        await connection
-        console.log("server started")
-    } catch (err) {
-        console.log(err)
-    }
-})
+  try {
+    await connection;
+    console.log("server started");
+  } catch (err) {
+    console.log(err);
+  }
+});
